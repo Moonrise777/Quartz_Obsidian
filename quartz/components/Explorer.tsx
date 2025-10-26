012be009f87df10cd3ba48090e536dd30d66e2cd
Arely <onixuniverse@gmail.com>
2025-10-25 23:12:08 -0600

    Cambios en explorer revertidos

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -1,8 +1,6 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/explorer.scss"
 
-import { useState } from "preact/hooks"
-
 // @ts-ignore
 import script from "./scripts/explorer.inline"
 import { classNames } from "../util/lang"
@@ -63,9 +61,6 @@ export default ((userOpts?: Partial<Options>) => {
   const { OverflowList, overflowListAfterDOMLoaded } = OverflowListFactory()
 
   const Explorer: QuartzComponent = ({ cfg, displayClass }: QuartzComponentProps) => {
-
-    const [explorerHidden, setExplorerHidden] = useState(false)
-
     const id = `explorer-${numExplorers++}`
 
     return (
@@ -102,32 +97,31 @@ export default ((userOpts?: Partial<Options>) => {
             <line x1="4" x2="20" y1="18" y2="18" />
           </svg>
         </button>
-        
         <button
           type="button"
           class="title-button explorer-toggle desktop-explorer"
           data-mobile={false}
-          aria-expanded={!explorerHidden} // <--- 1. CAMBIADO
-          onClick={() => setExplorerHidden(!explorerHidden)} // <--- 2. AÑADIDO
+          aria-expanded={true}
         >
-          {/* 3. El <svg> de la flecha se ha borrado */}
           <h2>{opts.title ?? i18n(cfg.locale).components.explorer.title}</h2>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="5 8 14 8"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="fold"
+          >
+            <polyline points="6 9 12 15 18 9"></polyline>
+          </svg>
         </button>
-        
-        
-
-
-        <div 
-          id={id} 
-          class={classNames("explorer-content", explorerHidden && "explorer-hidden")} // <--- 1. CAMBIADO
-          aria-expanded={!explorerHidden} // <--- 2. CAMBIADO
-          role="group"
-        >
+        <div id={id} class="explorer-content" aria-expanded={false} role="group">
           <OverflowList class="explorer-ul" />
         </div>
-
-
-
         <template id="template-file">
           <li>
             <a href="#"></a>
